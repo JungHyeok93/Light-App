9cd6af4e8035b572a7cf85538eff5c66d16973c6
Se Jin Lee <sjleesogang@gmail.com>
2018-03-28 14:11:20 +0900

    background color

--- FirstApp3/ViewController.swift
+++ FirstApp3/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
-    let messageArray  = ["May the force be with you","Live long and prosper","To infinity and beyond","Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is",10] as [Any]
-    var index=0
-    
+   /* let messageArray  = ["May the force be with you","Live long and prosper","To infinity and beyond","Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is",10] as [Any]
+    var index=0*/
+    var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,11 +27,22 @@ class ViewController: UIViewController {
 
     @IBAction func doButtonTap(_ sender: UIButton) {
         print("Button Touched")
-        if let nextString = self.messageArray[index] as? String {
+        
+        if lightOn == true{
+            self.messageLabel.text = "White"
+            view.backgroundColor = UIColor.black
+        }
+        else {
+            self.messageLabel.text = "Black"
+            view.backgroundColor = UIColor.white
+        }
+        lightOn = !lightOn
+        
+       /* if let nextString = self.messageArray[index] as? String {
             self.messageLabel.text = nextString
         }
         index = index + 1
-        index %= self.messageArray.count
+        index %= self.messageArray.count*/
     }
 
 }
