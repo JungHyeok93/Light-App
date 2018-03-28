603f96a1561d45843e5702109f54305fac835a29
Se Jin Lee <sjleesogang@gmail.com>
2018-03-28 14:34:57 +0900

    final SW module

--- FirstApp3/ViewController.swift
+++ FirstApp3/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
-   /* let messageArray  = ["May the force be with you","Live long and prosper","To infinity and beyond","Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is",10] as [Any]
+    @IBOutlet weak var messageButton: UIButton!
+    /* let messageArray  = ["May the force be with you","Live long and prosper","To infinity and beyond","Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is",10] as [Any]
     var index=0*/
     var lightOn = true
     override func viewDidLoad() {
@@ -27,23 +28,32 @@ class ViewController: UIViewController {
 
     @IBAction func doButtonTap(_ sender: UIButton) {
         print("Button Touched")
-        
+        updateUI()
+        lightOn = !lightOn
+    }
+    
+    //Cleaning
+    func updateUI() {
         if lightOn == true{
             self.messageLabel.text = "White"
+            self.messageLabel.textColor = UIColor.white
             view.backgroundColor = UIColor.black
+            self.messageButton.setTitle("Off", for: .normal)
         }
         else {
             self.messageLabel.text = "Black"
+            self.messageLabel.textColor = UIColor.black
             view.backgroundColor = UIColor.white
+            self.messageButton.setTitle("On", for: .normal)
         }
-        lightOn = !lightOn
+    }
         
        /* if let nextString = self.messageArray[index] as? String {
             self.messageLabel.text = nextString
         }
         index = index + 1
         index %= self.messageArray.count*/
-    }
-
 }
+
+
 
